3943b562a9052e3748ba2a710f466504aa752b52
Felix-Young-Git <91382176+Felix-Young-Git@users.noreply.github.com>
2026-02-10 09:08:03 -0500

    Some of the code to TopSecret done, added tests folder

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,5 +1,61 @@
 /**
  * Commmand Line Utility
  */
+//This is the class that the command line stuff will be built in
 public class TopSecret {
+
+    //Fields
+    private String cipher;
+    private int fileNumber;
+
+    //Constructors for 0-2 parameters
+    public TopSecret() {
+        this.fileNumber = -1;   //-1 indicates that no file was specified
+        this.cipher = "";       //Empty string indicates no cipher was specified
+
+        //Call printFileList?
+    }
+
+    public TopSecret(int fileNumber) {
+        this.fileNumber = fileNumber;
+        this.cipher = "";       //Empty string indicates no cipher was specified
+
+        //Call printFile(fileNumber, cipher)?
+    }
+
+    public TopSecret(int fileNumber, String cipher) {
+        this.fileNumber = fileNumber;
+        this.cipher = cipher;
+
+        //Call printFile(fileNumber, cipher)?
+    }
+
+
+    //TODO - print list of files
+    public void printFileList(int fileNumber, String cipher){
+        //Print the list of files
+    }
+
+    //TODO - print specified file
+    public void printFile(int fileNumber, String cipher){
+        //Print the specified file
+    }
+
+
+    //Getters and Setters
+    public String getCipher() {
+        return cipher;
+    }
+
+    public int getFileNumber() {
+        return fileNumber;
+    }
+
+    public void setCipher(String cipher) {
+        this.cipher = cipher;
+    }
+
+    public void setFileNumber(int fileNumber) {
+        this.fileNumber = fileNumber;
+    }
 }
