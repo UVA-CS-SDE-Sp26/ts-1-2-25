ec69c50c4b791051e0845d04083f51259bc17d82
Felix-Young-Git <91382176+Felix-Young-Git@users.noreply.github.com>
2026-02-10 12:22:58 -0500

    TopSecret main method completed

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,8 +1,11 @@
+import static java.sql.Types.NULL;
+
 /**
  * Commmand Line Utility
  */
 //This is the class that the command line stuff will be built in
 public class TopSecret {
+
 
     //Fields
     private String cipher;
@@ -10,35 +13,71 @@ public class TopSecret {
 
     //Constructors for 0-2 parameters
     public TopSecret() {
-        this.fileNumber = -1;   //-1 indicates that no file was specified
+        this.fileNumber = -1;   //-1 indicates that no file was specified, but I don't think these matter as they arn't being used
         this.cipher = "";       //Empty string indicates no cipher was specified
-
-        //Call printFileList?
     }
 
     public TopSecret(int fileNumber) {
         this.fileNumber = fileNumber;
         this.cipher = "";       //Empty string indicates no cipher was specified
-
-        //Call printFile(fileNumber, cipher)?
     }
 
     public TopSecret(int fileNumber, String cipher) {
         this.fileNumber = fileNumber;
         this.cipher = cipher;
-
-        //Call printFile(fileNumber, cipher)?
     }
 
+    //Main
+    public static void main(String[] args){
+        //Determine # of params and what to do from there
+        //No params
+        if(args.length == NULL){
+            TopSecret listFiles = new TopSecret();
+            listFiles.printFileList();
+
+        //1 param - check if first is a number
+        } else if (args.length == 1) {
+            int fileNumber = isInteger(args[0]);
+            if(fileNumber < 1) {
+                System.out.println("You entered: " + args[0] + " as the first parameter, but it must a number that is at least 1.");
+            }
+            TopSecret displayFile = new TopSecret(fileNumber);
+            displayFile.printFile(displayFile.getFileNumber(), displayFile.getCipher());
+
+        //2 params - check if first is number, -----check if second is cipher???
+        } else if (args.length == 2) {
+            int fileNumber = isInteger(args[0]);
+            if(fileNumber < 1) {
+                System.out.println("You entered: " + args[0] + " as the first parameter, but it must a number that is at least 1.");
+            }
+            TopSecret displayCipheredFile = new TopSecret(fileNumber, args[1]);
+            displayCipheredFile.printFile(displayCipheredFile.getFileNumber(), displayCipheredFile.getCipher());
+
+        //Some wrong number of params
+        }else{
+            System.out.println("Sorry this program accepts up to 2 parameters, first a number, and second a string");
+        }
+
+    }
+
+    //Try and convert the string to int and return, otherwise, return -1, indicating an error
+    public static int isInteger(String str) {
+        try{
+            int fileNumber = Integer.parseInt(str);
+            return fileNumber;
+        }catch (NumberFormatException e){
+            return -1;
+        }
+    }
 
     //TODO - print list of files
-    public void printFileList(int fileNumber, String cipher){
+    public void printFileList(){
         //Print the list of files
     }
 
     //TODO - print specified file
     public void printFile(int fileNumber, String cipher){
-        //Print the specified file
+        //Print the specified file - if cipher is "" then no cipher was specified
     }
 
 
--- src/main/tests/TopSecretTest.java
+++ src/main/tests/TopSecretTest.java
@@ -4,15 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TopSecretTest {
 
+    //Testing the function to print a list of files
     @Test
     void printFileList() {
         TopSecret testFileList = new TopSecret();
         assertEquals("", testFileList, "");
     }
 
+    //Testing the function to print a specific file
     @Test
     void printFile() {
         TopSecret testPrintFile = new TopSecret();
         assertEquals("", testPrintFile, "");
+    }
+
+    @Test
+    void main() {
+    }
+
+
+    @Test
+    void isInteger() {
     }
 }
