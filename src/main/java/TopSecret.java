cd32fa2998072fc2cff576c22aa3d99477fcc721
Felix-Young-Git <91382176+Felix-Young-Git@users.noreply.github.com>
2026-02-13 13:43:15 -0500

    Updated TopSecret so I could fix the issues with testing

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -20,38 +20,46 @@ public class TopSecret {
 
     //Main
     public static void main(String[] args){
+        run(args);
+    }
+
+    public static int run(String[] args){
         //Determine # of params and what to do from there
         //No params
         if(args.length == 0){
             TopSecret listFiles = new TopSecret(args);
             listFiles.printFileList();
 
-        //1 param - check if first is a number
+            //1 param - check if first is a number
         } else if (args.length == 1) {
             int fileNumber = isInteger(args[0]);
             System.out.println(fileNumber);
             if(fileNumber < 1) {
                 System.out.println("You entered: " + args[0] + " as the first parameter, but it must a number that is at least 1.");
-                return;
+                return -1;
             }
             TopSecret displayFile = new TopSecret(args);
             displayFile.printFile(args);
 
-        //2 params - check if first is number, -----check if second is cipher???
+            //2 params - check if first is number, -----check if second is cipher???
         } else if (args.length == 2) {
             int fileNumber = isInteger(args[0]);
             if(fileNumber < 1) {
                 System.out.println("You entered: " + args[0] + " as the first parameter, but it must a number that is at least 1.");
-                return;
+                return -1;
             }
             TopSecret displayCipheredFile = new TopSecret(args);
             displayCipheredFile.printFile(args);
 
-        //Some wrong number of params
+            //Some wrong number of params
         }else{
             System.out.println("Sorry this program accepts up to 2 parameters, first a number, and second a string");
+            return -1;
         }
+        return 0;
     }
+
+
 
     //Try and convert the string to int and return, otherwise, return -1, indicating an error
     public static int isInteger(String str) {
--- src/test/java/TopSecretTest.java
+++ src/test/java/TopSecretTest.java
@@ -8,53 +8,64 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TopSecretTest {
-    private final PrintStream standardOut = System.out;
-    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-    @BeforeEach
-    public void setUp(){
-        outputStreamCaptor.reset();
-        System.setOut(new PrintStream(outputStreamCaptor));
-    }
-    @AfterEach
-    void reset(){
-        System.setOut(standardOut);
-    }
+ //   private final PrintStream standardOut = System.out;
+  //  private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+  //  @BeforeEach
+  //  public void setUp(){
+  //      outputStreamCaptor.reset();
+   //     System.setOut(new PrintStream(outputStreamCaptor));
+   // }
+   // @AfterEach
+   // void reset(){
+    //    System.setOut(standardOut);
+   // }
 
 
     //Test 0, 1, and 2 parameters, and with invalid inputs - the easiest way seemed to be with streams
     @Test
     void mainTestNoArgs() {
         String[] args = {};
-        TopSecret.main(args);
-        assertEquals("", outputStreamCaptor.toString().trim());
+        assertEquals(0, TopSecret.run(args), "This should run fine, so it should return 0");
+        //String[] args = {};
+       // TopSecret.main(args);
+        //assertEquals("", outputStreamCaptor.toString().trim());
 
     }
 
     @Test
     void mainTestOneArgValid() {
-        String[] args = {"3"};
-        TopSecret.main(args);
-        assertEquals("3", outputStreamCaptor.toString().trim());
+        String[] args = {"1"};
+        assertEquals(0, TopSecret.run(args), "This should run fine, so it should return 0");
+       // String[] args = {"3"};
+       // TopSecret.main(args);
+       // assertEquals("3", outputStreamCaptor.toString().trim());
     }
 
     @Test
     void mainTestOneArgInvalid() {
         String[] args = {"a"};
-        TopSecret.main(args);
-        assertTrue(outputStreamCaptor.toString().contains("must a number"));
+        assertEquals(-1, TopSecret.run(args), "This should not run fine, so it should return -1");
+       // String[] args = {"a"};
+       // TopSecret.main(args);
+        //assertTrue(outputStreamCaptor.toString().contains("must a number"));
     }
     @Test
     void mainTestTwoArgValid() {
-        String[] args = {"1", "cdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890ab"};
-        TopSecret.main(args);
-        assertEquals("ERROR: Invalid key", outputStreamCaptor.toString().trim());
+        String[] args = {"1", "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890\n" +
+                "bcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890a"};
+        assertEquals(0, TopSecret.run(args), "This should run fine, so it should return 0");
+        //String[] args = {"1", "cdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890ab"};
+       // TopSecret.main(args);
+        //assertEquals("ERROR: Invalid key", outputStreamCaptor.toString().trim());
     }
 
     @Test
     void mainTestTwoArgInvalid() {
-        String[] args = {"a", "aaaa"};
-        TopSecret.main(args);
-        assertTrue(outputStreamCaptor.toString().contains("must a number"));
+        String[] args = {"a", "a"};
+        assertEquals(-1, TopSecret.run(args), "This should not run fine, so it should return -1");
+        //String[] args = {"a", "aaaa"};
+        //TopSecret.main(args);
+        //assertTrue(outputStreamCaptor.toString().contains("must a number"));
     }
 
     //Test constructor
